4e580d7d3839daedf964054a705474a3061a9230
Elmira <bbelyab@gmail.com>
2021-09-11 01:55:26 +0300

    First task done

---
+++ itis.playground/Pages/homework.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//First task
+
+
+class Transport {
+    let model: String
+
+    init(model: String) {
+        self.model = model
+    }
+    
+    //Инкапсуляция
+    func takeoff() {
+        fatalError("This method can only be executed for plane")
+    }
+}
+
+//Наследование
+class Aircraft: Transport {
+    let isBroken: Bool
+    
+    init(model: String, isBroken: Bool = false) {
+        self.isBroken = isBroken
+        
+        super.init(model: model)
+    }
+    
+    override func takeoff() {
+        print("Aircraft took off")
+    }
+}
+
+//Полиморфзим
+class Plane: Aircraft {
+    override func takeoff() {
+        print("Plane took off")
+    }
+}
+//: [Next](@next)
