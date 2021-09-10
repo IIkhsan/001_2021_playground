54118aab3f69a5d0568eb0f3860c47a718f1f1fd
Daniil <daniilbagautdinov@yandex.ru>
2021-09-10 23:02:14 +0300

    added the first and second tasks

--- itis.playground/Pages/task1.xcplaygroundpage/Contents.swift
+++ itis.playground/Pages/task1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,70 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+//абстракция
+protocol Phone {
+    var number: Int { get }
+    
+    init(number: Int)
+    
+    func call(outputNumber: Int)
+    
+    func ring(inputNumber: Int)
+}
+
+
+//инкапсуляция
+class SomePhone {
+    private var year: Int
+    private var company: String
+    
+    
+    init(year: Int, company: String) {
+        self.year = year
+        self.company = company
+    }
+    
+    private func openConnection(){}
+    
+    func call() {
+        openConnection()
+        print("Вызов")
+    }
+    
+    func ring() {
+        print("Звонок")
+    }
+}
+
+
+//наследование
+class Iphone: Phone {
+    var number: Int
+    
+    required init(number: Int) {
+        self.number = number
+    }
+    
+    func call(outputNumber: Int) {
+        print("Вызов по номеру\(outputNumber)")
+    }
+    
+    func ring(inputNumber: Int) {
+        print("Звонок от \(inputNumber)")
+    }
+}
+
+
+
+//полиморфизм
+class Iphone4s: Iphone {
+    func daf() {
+        print("sd")
+    }
+}
+
+var iphone: Iphone = Iphone4s(number: 4)
+
+print(iphone.self)
+iphone.call(outputNumber: 1234)
--- itis.playground/Pages/task2.xcplaygroundpage/Contents.swift
+++ itis.playground/Pages/task2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,78 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct Drob {
+    var numerator: Int
+    var denominator: Int
+    
+    init(numerator: Int, denominator: Int) {
+        self.numerator = numerator
+        self.denominator = denominator
+    }
+    
+    func show(){
+        print("\(numerator)/\(denominator)")
+    }
+    
+    
+    func NOD() -> Drob{
+        var a: Int = numerator
+        var b: Int = denominator
+        if a > 0 && b > 0 {
+            while (a != b){
+                if a > b {
+                    a = a - b
+                }else {
+                    b = b - a
+                }
+            }
+        }else {
+            a *= -1
+            while a != b {
+                if a > b {
+                    a = a - b
+                }else {
+                    b = b - a
+                }
+            }
+        }
+        
+        
+        
+        return Drob(numerator: numerator/a , denominator: denominator/b)
+    }
+}
 
-//: [Next](@next)
+struct Calculator {
+    var drob1: Drob
+    var drob2: Drob
+    
+    init(drob1: Drob, drob2: Drob) {
+        self.drob1 = drob1
+        self.drob2 = drob2
+    }
+    
+    func addition() -> Drob {
+        let resultDenominator = drob1.denominator*drob2.denominator;
+        let resultNumerator = resultDenominator/drob1.denominator*drob1.numerator + resultDenominator/drob2.denominator*drob2.numerator
+        return Drob(numerator: resultNumerator, denominator: resultDenominator).NOD()
+    }
+    
+    func subtraction() -> Drob {
+        let resultDenominator = drob1.denominator*drob2.denominator;
+        let resultNumerator = resultDenominator/drob1.denominator*drob1.numerator - resultDenominator/drob2.denominator*drob2.numerator
+        return Drob(numerator: resultNumerator, denominator: resultDenominator).NOD()
+    }
+    
+    func multiplication() -> Drob {
+        return Drob(numerator: drob1.numerator*drob2.numerator, denominator: drob1.denominator*drob2.denominator).NOD()
+    }
+    
+    func division() -> Drob {
+        return Drob(numerator: drob1.numerator*drob2.denominator, denominator: drob1.denominator*drob2.numerator).NOD()
+    }
+}
+
+
+print(Calculator(drob1: Drob(numerator: 12, denominator: 100), drob2: Drob(numerator: 23, denominator: 56)).division())
+
