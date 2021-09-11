0f40f4a9b642247130ce610f8ec0ffe38b0f720f
Daniil <daniilbagautdinov@yandex.ru>
2021-09-11 18:25:11 +0300

    complited third task

--- itis.playground/Pages/task3.xcplaygroundpage/Contents.swift
+++ itis.playground/Pages/task3.xcplaygroundpage/Contents.swift
@@ -5,26 +5,29 @@ import Foundation
 class Fraction {
     var name: String
     var students: [Student]
-    init(name: String, students: [Student]) {
+    var id: Int
+    init(name: String, students: [Student], id: Int) {
         self.name = name
         self.students = students
+        self.id = id
     }
 }
 
 
 enum Weapon: Int{
-    case pistol = 5
-    case shotgun = 7
-    case rifle = 9
+    case pen = 5
+    case razor = 7
+    case stone = 9
 }
 
-protocol Student {
+protocol Student : AnyObject {
     var hp: Int { get set }
     var weapon: Weapon? { get set }
     var damage: Int { get set }
     var chant: String { get set }
     var name: String { get set }
 }
+
 protocol Arena {
     var fraction1: Fraction { get set }
     var fraction2: Fraction { get set }
@@ -35,8 +38,8 @@ protocol Arena {
 
 
 class highLevelStudent: Student {
-    var weapon: Weapon?{
-        didSet{
+    var weapon: Weapon? {
+        didSet {
             guard let weaponDamage = weapon?.rawValue else {return}
             damage = weaponDamage
         }
@@ -46,7 +49,7 @@ class highLevelStudent: Student {
     
     var damage: Int = 3
     
-    var chant: String = "В АТАКУ!!!"
+    var chant: String = "Я НЕ ПОПБЕДИМ!!!"
     
     var name: String
     
@@ -57,8 +60,8 @@ class highLevelStudent: Student {
 
 class middleLevelStudent: Student {
     
-    var weapon: Weapon?{
-        didSet{
+    var weapon: Weapon? {
+        didSet {
             guard let weaponDamage = weapon?.rawValue else {return}
             damage = weaponDamage
         }
@@ -68,7 +71,7 @@ class middleLevelStudent: Student {
     
     var damage: Int = 2
     
-    var chant: String = "ГРАААААААА!!!"
+    var chant: String = "БАТЯ В ЗДАНИИ!!!"
     
     var name: String
     
@@ -80,8 +83,8 @@ class middleLevelStudent: Student {
 
 class lowLevelStudent: Student {
     
-    var weapon: Weapon?{
-        didSet{
+    var weapon: Weapon? {
+        didSet {
             guard let weaponDamage = weapon?.rawValue else {return}
             damage = weaponDamage
         }
@@ -91,7 +94,7 @@ class lowLevelStudent: Student {
     
     var damage: Int = 1
     
-    var chant: String = "Я ЕСТЬ ВОИИИИИН!!!"
+    var chant: String = "ЧИКИРЯУ!!!"
     
     var name: String
     
@@ -99,3 +102,133 @@ class lowLevelStudent: Student {
         self.name = name
     }
 }
+
+
+class Bully: Arena {
+    var fraction1: Fraction
+    
+    var fraction2: Fraction
+    
+    init(fraction1: Fraction, fraction2: Fraction) {
+        self.fraction1 = fraction1
+        self.fraction2 = fraction2
+    }
+    
+    func startBattle() {
+    
+        print()
+        print("Студенты разбежались, для того чтобы найти предметы, которые помогут им в схватке...")
+        lootStage(fraction: fraction1)
+        
+        lootStage(fraction: fraction2)
+        print()
+        
+        let looser = fightStage(fraction1: fraction1, fraction2: fraction2)
+        print()
+        var winner: Fraction
+        if looser.id != fraction1.id {
+            winner = fraction1
+        }else {
+            winner = fraction2
+        }
+        
+        let randomAffected = Int.random(in: 2...winner.students.count)
+        
+        print("Победителем становится фракция под названием \(winner.name)!!! Они потеряли \(randomAffected) членов фракции")
+        
+        
+    }
+    
+    func lootStage(fraction: Fraction) {
+        print()
+        print("Фракция \(fraction.name) лутается")
+        for student in fraction.students {
+            
+            let randomNumber = Int.random(in: 1...100)
+            if randomNumber>70 {
+                print("Кажется студент \(student.name) остался с пустыми руками. Кулаки в помощь!")
+                student.weapon = nil
+            }else if randomNumber>54 {
+                print("Студент по имени \(student.name) смог найти ручку")
+                student.weapon = .pen
+            }else if randomNumber>38 {
+                print("\(student.name) смог найти бритву. Похоже кому-то не поздоровится")
+                student.weapon = .razor
+            }else if randomNumber>20 {
+                print("Вот он победитель, ему крупно повезло. \(student.name) нашел камень. Берегитесь его")
+                student.weapon = .stone
+            }else {
+                print("Вы только посмотрите!!! №\(student.name) смог найти энергетик. Сейчас он востоновит все свои силы!")
+                student.hp += 3
+            }
+            print(student.chant)
+        }
+    }
+    
+    func fightStage(fraction1: Fraction, fraction2: Fraction) -> Fraction {
+        var losingFraction: Fraction = fraction1
+        print("Начало битвы между двумя фракциями: \(fraction1.name) и \(fraction2.name)")
+        
+        let sumHP1 = sumHP(fraction: fraction1)
+        let sumDmg1 = sumDmg(fraction: fraction1)
+        let sumHP2 = sumHP(fraction: fraction2)
+        let sumDmg2 = sumDmg(fraction: fraction2)
+        
+        if sumHP1==sumHP2 && sumDmg1>sumDmg2 {
+            losingFraction = fraction2
+        }else if sumHP1==sumHP2 && sumDmg1<sumDmg2 {
+            losingFraction = fraction1
+        }else if sumDmg1 == sumDmg2 && sumHP1>sumHP2 {
+            losingFraction = fraction2
+        }else if sumDmg1 == sumDmg2 && sumHP1<sumHP2 {
+            losingFraction = fraction1
+        }else if sumHP1>sumHP2 && sumDmg1>sumDmg2 {
+            losingFraction = fraction2
+        }else if sumHP1>sumHP2 && sumDmg1<sumDmg2 {
+            let randomNumber = Int.random(in: 1...100)
+            if randomNumber > 50{
+                losingFraction = fraction1
+            }else {
+                losingFraction = fraction2
+            }
+        }else if sumHP1<sumHP2 && sumDmg1>sumDmg2 {
+            let randomNumber = Int.random(in: 1...100)
+            if randomNumber > 50{
+                losingFraction = fraction2
+            }else {
+                losingFraction = fraction1
+            }
+        }else if sumHP1<sumHP2 && sumDmg1<sumDmg2 {
+            losingFraction = fraction1
+        }
+        print("Фракция \(losingFraction.name) уничтожена и у нас определился победитель в этой схватке!")
+        
+        return losingFraction
+    }
+    
+    func sumHP(fraction: Fraction) -> Int {
+        var result:Int = 0
+        for student in fraction.students {
+            result += student.hp
+        }
+        
+        return result
+    }
+    
+    func sumDmg(fraction: Fraction) -> Int {
+        var result:Int = 0
+        for student in fraction.students {
+            result += student.damage
+        }
+        
+        return result
+    }
+    
+}
+
+
+
+
+var fraction1 = Fraction (name: "Акробаты", students: [highLevelStudent (name: "Даня"), middleLevelStudent (name: "Саня"), middleLevelStudent (name: "Фаня"), lowLevelStudent (name: "Лера")], id: 1)
+var fraction2 = Fraction (name: "Лесорубы", students: [highLevelStudent (name: "Тыква"), middleLevelStudent (name: "Арбуз"), middleLevelStudent (name: "Шога"), lowLevelStudent (name: "Нина")], id: 2)
+Bully (fraction1: fraction1, fraction2: fraction2).startBattle()
