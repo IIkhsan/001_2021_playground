84a4e3828902e0a0d381e1f694f779cef9b9b134
Ruslan <ruslangazizov36@gmail.com>
2021-09-11 22:17:20 +0300

    Add ZeroDivisionError in fractional number task

--- itis.playground/Pages/fractional_number.xcplaygroundpage/Contents.swift
+++ itis.playground/Pages/fractional_number.xcplaygroundpage/Contents.swift
@@ -2,6 +2,10 @@
 
 import Foundation
 
+enum MyErrors: Error {
+    case ZeroDivisionError(String)
+}
+
 struct Fraction: CustomStringConvertible, Equatable {
     
     var numerator: Int
@@ -12,6 +16,14 @@ struct Fraction: CustomStringConvertible, Equatable {
             return String(numerator)
         }
         return String(numerator) + "/" + String(denominator)
+    }
+    
+    init(numerator: Int, denominator: Int) throws {
+        if denominator == 0 {
+            throw MyErrors.ZeroDivisionError("Division by zero")
+        }
+        self.numerator = numerator
+        self.denominator = denominator
     }
     
     func reduce() -> Fraction {
@@ -26,11 +38,11 @@ struct Fraction: CustomStringConvertible, Equatable {
         let newNumerator = numerator / num_copy
         let newDenominator = denominator / num_copy
         
-        return Fraction(numerator: newNumerator, denominator: newDenominator)
+        return try! Fraction(numerator: newNumerator, denominator: newDenominator)
     }
     
     mutating func reduceSelf() {
-        let fraction = Fraction(numerator: numerator, denominator: denominator).reduce()
+        let fraction = try! Fraction(numerator: numerator, denominator: denominator).reduce()
         numerator = fraction.numerator
         denominator = fraction.denominator
     }
@@ -40,7 +52,7 @@ struct Fraction: CustomStringConvertible, Equatable {
         let numerator = (denominator / left.denominator) * left.numerator +
             (denominator / right.denominator) * right.numerator
         
-        return Fraction(numerator: numerator, denominator: denominator).reduce()
+        return try! Fraction(numerator: numerator, denominator: denominator).reduce()
     }
     
     static func += (left: inout Fraction, right: Fraction) {
@@ -52,7 +64,7 @@ struct Fraction: CustomStringConvertible, Equatable {
         let numerator = (denominator / left.denominator) * left.numerator -
             (denominator / right.denominator) * right.numerator
         
-        return Fraction(numerator: numerator, denominator: denominator).reduce()
+        return try! Fraction(numerator: numerator, denominator: denominator).reduce()
     }
     
     static func -= (left: inout Fraction, right: Fraction) {
@@ -63,7 +75,7 @@ struct Fraction: CustomStringConvertible, Equatable {
         let numerator = left.numerator * right.numerator
         let denominator = left.denominator * right.denominator
         
-        return Fraction(numerator: numerator, denominator: denominator).reduce()
+        return try! Fraction(numerator: numerator, denominator: denominator).reduce()
     }
     
     static func *= (left: inout Fraction, right: Fraction) {
@@ -74,7 +86,7 @@ struct Fraction: CustomStringConvertible, Equatable {
         let numerator = left.numerator * right.denominator
         let denominator = left.denominator * right.numerator
         
-        return Fraction(numerator: numerator, denominator: denominator).reduce()
+        return try! Fraction(numerator: numerator, denominator: denominator).reduce()
     }
     
     static func /= (left: inout Fraction, right: Fraction) {
@@ -90,8 +102,8 @@ struct Fraction: CustomStringConvertible, Equatable {
 
 func main() {
     
-    let fraction1 = Fraction(numerator: 5, denominator: 4)
-    let fraction2 = Fraction(numerator: 3, denominator: 4)
+    let fraction1 = try! Fraction(numerator: 5, denominator: 4)
+    let fraction2 = try! Fraction(numerator: 3, denominator: 4)
     
     print("fraction1 = \(fraction1)")
     print("fraction2 = \(fraction2)")
