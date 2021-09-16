e6b29b44ed98a9d35b8dd9fce56461f42ab6aba2
AndrewOch <38253789+AndrewOch@users.noreply.github.com>
2021-09-16 09:33:23 +0300

    code style review

--- itis.playground/Pages/homework.xcplaygroundpage/Contents.swift
+++ itis.playground/Pages/homework.xcplaygroundpage/Contents.swift
@@ -1,20 +1,20 @@
 
 import Foundation
 
-//MARK OOP
-class User{
+//MARK: - OOP
+class User {
     
-    var name:String
-    var email:String
-    var age:Int
+    var name: String
+    var email: String
+    var age: Int
     
-    init(_ name:String,_ email:String,_ age:Int) {
-        self.name=name
-        self.email=email
-        self.age=age
+    init(name: String, email: String, age: Int) {
+        self.name = name
+        self.email = email
+        self.age = age
     }
     
-    func repostAction(action:Action) {
+    func repostAction(action: Action) {
         print(name," reposted: \n",action.show())
     }
     
@@ -28,13 +28,13 @@ class User{
     
 }
 
-class Admin: User{
+class Admin: User {
     
     var adminLevel:Int
     
-    init(_ name:String,_ email:String,_ age:Int,_ adminLevel:Int) {
+    init(name: String, email: String, age: Int, adminLevel: Int) {
         self.adminLevel = adminLevel
-        super.init(name, email, age)
+        super.init(name: name, email: email, age: age)
     }
     
     override func greeting(){
@@ -42,78 +42,78 @@ class Admin: User{
     }
 }
 
-class Action{
-    var user:User
+class Action {
+    var user: User
     
-    init(_ user:User) {
+    init(user: User) {
         self.user = user
     }
     
     func show(){}
 }
 
-class Message:Action{
-    var text:String
+class Message: Action {
+    var text: String
     
-    init(_ user:User,_ text:String) {
+    init(user: User, text: String) {
         self.text = text
-        super.init(user)
+        super.init(user: user)
     }
     
     override func show() {
-        print(user.name,"sent a message: ",text)
+        print(user.name, "sent a message: ", text)
     }
     
 }
 
-class Image:Action{
-    var verticalPixels:Int
-    var horizontalPixels:Int
+class Image: Action {
+    var verticalPixels: Int
+    var horizontalPixels: Int
     
-    init(_ user:User, _ vertical:Int,_ horizontal:Int) {
+    init(user: User, vertical: Int, horizontal: Int) {
         self.verticalPixels = vertical
         self.horizontalPixels = horizontal
-        super.init(user)
+        super.init(user: user)
     }
     
     override func show() {
-        print(user.name,"sent an image. Its resolution is",horizontalPixels,"*",verticalPixels)
+        print(user.name, "sent an image. Its resolution is", horizontalPixels, "*", verticalPixels)
     }
     
 }
 
-var u1 = User("Peter", "efeeev", 13)
-var u2 = Admin("Terence", "fgrg", 30,2)
-var act1 = Image(u1, 1920,1080)
+var u1 = User(name: "Peter", email: "efeeev", age: 13)
+var u2 = Admin(name: "Terence", email: "fgrg", age: 30, adminLevel: 2)
+var act1 = Image(user: u1, vertical: 1920, horizontal: 1080)
 u2.greeting(anotherUser: u1)
 u2.repostAction(action: act1)
 
 
-//MARK fractions
+//MARK: - fractions
 struct Fraction {
-    var numerator:Int
-    var denominator:Int
+    var numerator: Int
+    var denominator: Int
     
-    init(_ numerator:Int,_ denominator:Int) {
+    init(numerator: Int, denominator: Int) {
         self.numerator = numerator
         self.denominator = denominator
     }
     
     func show() {
-        print(numerator,"/",denominator)
+        print(numerator, "/", denominator)
         
     }
     
-    static func +(_ left: Fraction, _ right: Fraction) -> Fraction {
+    static func +(_ left: Fraction,_ right: Fraction) -> Fraction {
         
         var editedLeft = left
         var editedRight = right
         if left.denominator != right.denominator {
-            editedLeft = Fraction(left.numerator * right.denominator, left.denominator * right.denominator)
-            editedRight = Fraction(right.numerator * left.denominator,right.denominator * left.denominator)
+            editedLeft = Fraction(numerator: left.numerator * right.denominator, denominator: left.denominator * right.denominator)
+            editedRight = Fraction(numerator: right.numerator * left.denominator,denominator: right.denominator * left.denominator)
         }
         
-        var newFraction = Fraction(editedLeft.numerator + editedRight.numerator, editedLeft.denominator)
+        var newFraction = Fraction(numerator: editedLeft.numerator + editedRight.numerator, denominator: editedLeft.denominator)
         newFraction.NOD()
         return newFraction
     }
@@ -123,25 +123,25 @@ struct Fraction {
         var editedLeft = left
         var editedRight = right
         if left.denominator != right.denominator {
-            editedLeft = Fraction(left.numerator * right.denominator, left.denominator * right.denominator)
-            editedRight = Fraction(right.numerator * left.denominator,right.denominator * left.denominator)
+            editedLeft = Fraction(numerator: left.numerator * right.denominator, denominator: left.denominator * right.denominator)
+            editedRight = Fraction(numerator: right.numerator * left.denominator,denominator: right.denominator * left.denominator)
         }
         
-        var newFraction = Fraction(editedLeft.numerator - editedRight.numerator, editedLeft.denominator)
+        var newFraction = Fraction(numerator: editedLeft.numerator - editedRight.numerator, denominator: editedLeft.denominator)
         newFraction.NOD()
         return newFraction
     }
     
     static func *(_ left: Fraction, _ right: Fraction) -> Fraction {
         
-        var newFraction = Fraction(left.numerator * right.numerator, left.denominator*right.denominator)
+        var newFraction = Fraction(numerator: left.numerator * right.numerator, denominator: left.denominator*right.denominator)
         newFraction.NOD()
         return newFraction
     }
     
     static func /(_ left: Fraction, _ right: Fraction) -> Fraction {
         
-        var newFraction = Fraction(left.numerator * right.denominator, left.denominator*right.numerator)
+        var newFraction = Fraction(numerator: left.numerator * right.denominator, denominator: left.denominator*right.numerator)
         newFraction.NOD()
         return newFraction
     }
@@ -149,48 +149,48 @@ struct Fraction {
     
     //Сокращение дробей
     mutating func NOD() {
-        var a_a:Int = numerator
-        var b_b:Int = denominator
-        var buf:Int
-        while (b_b != 0) {
-            buf = b_b;
-            b_b = a_a % b_b;
-            a_a = buf;
+        var a: Int = numerator
+        var b: Int = denominator
+        var buf: Int
+        while (b != 0) {
+            buf = b;
+            b = a % b;
+            a = buf;
         }
-        numerator = numerator / a_a
-        denominator = denominator / a_a
+        numerator = numerator / a
+        denominator = denominator / a
     }
 }
 
-var f1:Fraction = Fraction(1,2)
+var f1: Fraction = Fraction(numerator: 1,denominator: 2)
 f1.show()
 
-var f2:Fraction = Fraction(1, 3)
+var f2: Fraction = Fraction(numerator: 1, denominator: 3)
 
-var f3:Fraction = f1 + f2
+var f3: Fraction = f1 + f2
 f3.show()
 
 
-//MARK Bully
-protocol Student{
-    var name:String { get set }
-    var health:Int { get set }
-    var damage:Int { get set }
-    var scream:String {get set}
+//MARK: - Bully
+protocol Student {
+    var name: String { get set }
+    var health: Int { get set }
+    var damage: Int { get set }
+    var scream: String {get set}
     
-    func getAttacked(student:Student)
+    func getAttacked(student: Student)
     
     func showInfo()
 }
 
-class Player:Student{
+class Player: Student {
     
-    var name:String
-    var health:Int
-    var damage:Int
-    var scream:String
+    var name: String
+    var health: Int
+    var damage: Int
+    var scream: String
     
-    init(name:String,health:Int, damage:Int) {
+    init(name: String,health: Int, damage: Int) {
         self.name = name
         self.health = health
         self.damage = damage
@@ -199,28 +199,28 @@ class Player:Student{
     
     func getAttacked(student: Student) {
         if self.health > student.damage {
-            print(student.name," attacked ",self.name," and dealt ",student.damage," dmg")
+            print(student.name, " attacked ", self.name, " and dealt ", student.damage, " dmg")
             self.health -= student.damage
         }else{
-            print(student.name," attacked ",self.name," and dealt ",self.health," dmg")
+            print(student.name, " attacked ", self.name, " and dealt ",self.health, " dmg")
             self.health = 0
         }
     }
     
     func showInfo(){
-        print(self.name,":===============")
-        print(self.health,":hp  ",self.damage,":dmg")
+        print(self.name, ":===============")
+        print(self.health, ":hp  ", self.damage, ":dmg")
         print("======================")
     }
 }
 
-class Team{
+class Team {
     
-    var students:[Student]
-    var name:String
-    var scream:String
+    var students: [Student]
+    var name: String
+    var scream: String
     
-    init(name:String,scream:String,students:[Student]) {
+    init(name: String,scream: String,students: [Student]) {
         self.students = students
         self.scream = scream
         self.name = name
@@ -230,26 +230,26 @@ class Team{
     }
     
     func showInfo(){
-        print(self.students.count," students left")
+        print(self.students.count, " students left")
         for player in self.students {
             player.showInfo()
         }
     }
     
     func removeDeadPlayers(){
-        var alivePlayers:[Student] = []
+        var alivePlayers: [Student] = []
         for student in self.students {
             if student.health > 0{
                 alivePlayers.append(student)
             }else{
-                print("-\(student.name)  is dead :(")
+                print("-", student.name, " is dead :(")
             }
         }
         self.students = alivePlayers
     }
 }
 
-protocol Arena{
+protocol Arena {
     
     var teams: [Team] {get set}
     
@@ -257,52 +257,55 @@ protocol Arena{
     
 }
 
-class Forest:Arena{
+class Forest:Arena {
     var teams: [Team]
-    
-    func startBattle(){
-        var round = 1
-        while teams.count > 1 {
-            print("Round ",round," began!")
-            print(teams[0].name," : ",teams[0].scream)
-            print(teams[1].name," : ",teams[1].scream)
-            print("======================")
-            
-            for student in teams[0].students{
-                let target = teams[1].students.shuffled().first
-                target?.getAttacked(student: student)
-            }
-            for student in teams[1].students{
-                let target = teams[0].students.shuffled().first
-                target?.getAttacked(student: student)
-            }
-            
-            removeDeadTeams()
-            print("======================")
-            print("Round ",round," ended. Stats:\n")
-            showStats()
-            round+=1
-        }
-        print("Battle is over! Team",teams[0].name," won!!!")
-    }
-    
-   private func removeDeadTeams(){
-        var aliveTeams:[Team] = []
-        for team in teams {
-            team.removeDeadPlayers()
-            if team.students.count > 0 {
-                aliveTeams.append(team)
-            } else{
-                print("-\(team.name) is dead :(")
-            }
-        }
-        teams = aliveTeams
-    }
     
     init(firstTeam: Team, secondTeam: Team) {
         teams = []
         teams.append(firstTeam)
         teams.append(secondTeam)
+    }
+    
+    func startBattle(){
+        var round = 1
+        while teams.count > 1 {
+            let firstTeam = teams[0]
+            let secondTeam = teams[1]
+            
+            print("Round ", round, " began!")
+            print(firstTeam.name, " : ", firstTeam.scream)
+            print(secondTeam.name, " : ", secondTeam.scream)
+            print("======================")
+            
+            for student in firstTeam.students{
+                let target = secondTeam.students.shuffled().first
+                target?.getAttacked(student: student)
+            }
+            for student in secondTeam.students{
+                let target = firstTeam.students.shuffled().first
+                target?.getAttacked(student: student)
+            }
+            
+            removeDeadTeams()
+            print("======================")
+            print("Round ", round, " ended. Stats:\n")
+            showStats()
+            round+=1
+        }
+        print("Battle is over! Team", teams[0].name, " won!!!")
+    }
+    
+    private func removeDeadTeams(){
+        var aliveTeams: [Team] = []
+        for team in teams {
+            team.removeDeadPlayers()
+            if team.students.count > 0 {
+                aliveTeams.append(team)
+            } else{
+                print("-Team ", team.name, " is dead :(")
+            }
+        }
+        teams = aliveTeams
     }
     
     private func showStats(){
@@ -328,7 +331,7 @@ players2.append(Player(name: "Will", health: 100, damage: 5))
 players2.append(Player(name: "Leila", health: 100, damage: 9))
 
 
-var team1 = Team(name: "Speakers",scream: "go go speak speak!", students:players1)
+var team1 = Team(name: "Speakers",scream: "go go speak speak!", students: players1)
 var team2 = Team(name: "Bikers",scream: "bikes bikes for a rise!", students: players2)
 
 var forest = Forest(firstTeam: team1, secondTeam: team2)
